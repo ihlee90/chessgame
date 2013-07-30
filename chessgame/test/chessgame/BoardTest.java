0f5fa22cb606ed7e0733e24c5f805e1a6849d46f
ihlee at home <ihlee90@gmail.com>
2013-07-31 02:09:40 +0900

    Chap 2 -3 complete !

---
+++ chessgame/test/chessgame/BoardTest.java
@@ -0,0 +1,12 @@
+package chessgame;
+
+import junit.framework.TestCase;
+
+public class BoardTest extends TestCase {
+	public void testBoard() {
+		//Board vBoard = new Board();
+
+		//assertEquals(COLOR_WHITE, vBoard.getNumOf());
+
+	}
+	}
